53be5f9d8ed6ddffb06aefecddbfcbe08a0be85a
Torsten Schmits <torstenschmits@gmail.com>
2017-02-06 21:10:26 +0100

    type annos

--- integration/_support/base.py
+++ integration/_support/base.py
@@ -1,26 +1,26 @@
-from amino import Right
-from ribosome.test import PluginIntegrationSpec
-from ribosome.test.integration import ExternalIntegrationSpec
+from amino import Right, Either
+from ribosome.test.integration.spec import (ExternalIntegrationSpec,
+                                            PluginIntegrationSpec)
+from ribosome.test.integration.klk import VimIntegrationKlkHelpers
 
 from tubbs.logging import Logging
-from tubbs.test import Spec
 from tubbs.nvim_plugin import TubbsNvimPlugin
 
 
-class IntegrationCommon(Spec):
+class IntegrationCommon(VimIntegrationKlkHelpers):
 
     @property
-    def _prefix(self):
+    def _prefix(self) -> str:
         return 'tubbs'
 
     @property
-    def plugin_class(self):
+    def plugin_class(self) -> Either[str, type]:
         return Right(TubbsNvimPlugin)
 
 
 class TubbsIntegrationSpec(IntegrationCommon, ExternalIntegrationSpec):
 
-    def _start_plugin(self):
+    def _start_plugin(self) -> None:
         self.plugin.start_plugin()
         self._wait(.05)
         self._wait_for(lambda: self.vim.vars.p('started').present)
@@ -29,7 +29,7 @@ class TubbsIntegrationSpec(IntegrationCommon, ExternalIntegrationSpec):
 class TubbsPluginIntegrationSpec(IntegrationCommon, PluginIntegrationSpec,
                                  Logging):
 
-    def _start_plugin(self):
+    def _start_plugin(self) -> None:
         self._debug = True
         self.vim.cmd_sync('TubbsStart')
         self._pvar_becomes('started', True)
