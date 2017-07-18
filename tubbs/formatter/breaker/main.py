0d9b9a3a0a25469ad2a483f306eab27ac33b1aff
Torsten Schmits <torstenschmits@gmail.com>
2017-07-18 14:27:11 +0200

    weight break prios `< 1` by the line split ratio

--- tubbs/formatter/breaker/main.py
+++ tubbs/formatter/breaker/main.py
@@ -1,10 +1,12 @@
 import abc
+import math
 from typing import Callable, Tuple, Any
 
 from hues import huestr
 
 from amino import Either, List, L, Boolean, _, __, Maybe, Map, Eval, Right
 from amino.regex import Regex
+from amino.lazy import lazy
 
 from ribosome.nvim import NvimFacade
 from ribosome.util.callback import VimCallback
@@ -30,12 +32,14 @@ def only_ws(data: str) -> Boolean:
     return ws_re.match(data).present
 
 Handler = Callable[[], BreakCond]
+Z = Tuple[Breaks, List[str]]
 
 
 class BreakerBase(Formatter[BreakCond]):
 
-    def __init__(self, textwidth: int) -> None:
+    def __init__(self, textwidth: int, split_weight_variance: float=0.25) -> None:
         self.textwidth = textwidth
+        self.split_weight_variance = split_weight_variance
 
     @abc.abstractproperty
     def default_handler(self) -> Handler:
@@ -52,7 +56,7 @@ class BreakerBase(Formatter[BreakCond]):
         def folder(z: Tuple[Breaks, List[str]], a: Tuple[str, int]) -> Tuple[Breaks, List[str]]:
             state0, lines0 = z
             line, start = a
-            state1, lines1 = self.break_line(state0, line, start)
+            state1, lines1 = self.analyze_line(state0, line, start)
             return state1.reset(state0), lines0 + lines1
         def trim(lines: List[str]) -> List[str]:
             return lines.detach_head.map2(lambda h, t: t.map(__.strip()).cons(h.rstrip())) | lines
@@ -62,41 +66,68 @@ class BreakerBase(Formatter[BreakCond]):
             .fold_left((breaks, List()))(folder)[1]
         )
 
-    def break_line(self, breaks: Breaks, cur: str, start: int) -> Tuple[Breaks, List[str]]:
+    def analyze_line(self, breaks: Breaks, line: str, start: int) -> Z:
         def log_error(err: str) -> None:
             self.log.error(f'error in break conditions: {err}')
-        end = start + len(cur)
+        end = start + len(line)
         sub_breaks, qualified = breaks.range(start, end).leffect(log_error) | (breaks, List())
-        def rec2(sub: Breaks, data: str, pos: int) -> List[str]:
+        return (
+            self
+            .best_break(qualified, line, start)
+            .flat_map(L(self.break_line)(sub_breaks, _, line, start))
+            .leffect(self.log.ddebug)
+            .get_or_else((breaks, List(line)))
+        )
+
+    def best_break(self, breaks: List[StrictBreak], line: str, start: int) -> Either[str, StrictBreak]:
+        '''select the break with the highest prio.
+        if any forced breaks (>= 1.0) exist, use the highest one unconditionally.
+        otherwise, weight the prios with the evenness of the ratio by which they split the line, scoring highest for
+        0.5 split, using a gaussian with configurable variance.
+        '''
+        length = len(line)
+        def weighted_prio(b: StrictBreak) -> float:
+            return self._split_weight((b.position - start) / length)
+        return (
+            breaks
+            .filter(_.prio >= 1)
+            .max_by(_.prio)
+            .o(lambda: breaks.max_by(weighted_prio))
+            .to_either('no breaks for {}'.format(line))
+        )
+
+    def _split_weight(self, ratio: int) -> float:
+        val = ratio - 0.5
+        return math.exp(-(val * val) / self._split_coeff)
+
+    @lazy
+    def _split_coeff(self) -> float:
+        return 2 * self.split_weight_variance
+
+    def break_line(self, breaks: Breaks, brk: StrictBreak, line: str, start: int) -> Either[str, Z]:
+        def rec2(sub: Breaks, data: str, pos: int) -> Z:
             return (
-                List()
+                (sub, List(data))
                 if only_ws(data) else
-                self.break_line(sub, data, pos)
+                self.analyze_line(sub, data, pos)
             )
         def rec1(brk: StrictBreak) -> List[str]:
             pos = brk.position
             local_pos = pos - start
             self.log.ddebug('breaking at {}, {}'.format(pos, local_pos))
-            left = cur[:local_pos]
-            right = cur[local_pos:]
+            left = line[:local_pos]
+            right = line[local_pos:]
             self.log.ddebug(lambda: 'broke line into\n{}\n{}'.format(hl(left), hl(right)))
-            breaks1 = sub_breaks.apply(brk)
+            breaks1 = breaks.apply(brk)
             breaks_l, lines_l = rec2(breaks1, left, start)
             breaks_r, lines_r = rec2(breaks1.update(breaks_l), right, pos)
             return breaks_r, (lines_l + lines_r)
-        def rec0(brk: StrictBreak) -> Either[str, List[str]]:
-            msg = 'line did not exceed tw: {}'
-            return (
-                Boolean(len(cur) > self.textwidth or brk.prio >= 1.0)
-                .e(msg.format(cur), brk) /
-                rec1
-            )
-        broken = (
-            qualified.max_by(_.prio)
-            .to_either('no breaks for {}'.format(cur)) //
-            rec0
-        ).leffect(self.log.ddebug)
-        return broken | (breaks, List(cur))
+        msg = 'line did not exceed tw: {}'
+        return (
+            Boolean(len(line) > self.textwidth or brk.prio >= 1.0)
+            .e(msg.format(line), brk) /
+            rec1
+        )
 
     def handle(self, node: RoseAstTree, breaks: List[StrictBreak]) -> Either[str, List[CondBreak]]:
         handler = self.lookup_handler(node.data)
@@ -163,8 +194,7 @@ class VimDictBreaker(DictBreaker, VimCallback, metaclass=VimFormatterMeta):
         def convert(brk: Any) -> BreakCond:
             return (
                 tuple(brk)
-                if isinstance(brk, List) and
-                not brk.exists(L(isinstance)(_, List)) else
+                if isinstance(brk, List) and not brk.exists(L(isinstance)(_, List)) else
                 brk
             )
         return data.valmap(convert)
